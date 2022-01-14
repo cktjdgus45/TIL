4a2944578bdb6ec25b34b809602f28e12111e791
pooh <cktjdgus45@naver.com>
2022-01-14 23:22:12 +0900

    TIL - study middleware of express

--- express/app.js
+++ express/app.js
@@ -1,8 +1,31 @@
 import express from 'express';
 const app = express();
 
-app.get('/', (req, res, next) => {
-    res.send('hello world')
-});
+app.all('/api', (req, res, next) => {
+    console.log('all');
+    next();
+})
+app.get('/sky',
+    (req, res, next) => {
+        console.log('first');
+        // next(new Error('Error'));
+        next('use')
+    },
+    (req, res, next) => {
+        console.log('first2');
+        next();
+    },
+    (req, res, next) => {
+        console.log('first3');
+        res.send('end of middleware')
+    },
+)
 
+app.use((req, res, next) => {
+    res.status(404).send('Note available!');
+})
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(500).send('Sorry try later!');
+})
 app.listen(7777);
